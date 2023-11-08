ff63b08372b0215145465db399228bc35fbe74f4
Jose Sanchez <engineer@copydataai.com>
2023-11-08 17:58:21 +0000

    add: recursive inRangeASCII

--- src/utils/ascii.ts
+++ src/utils/ascii.ts
@@ -1,3 +1,4 @@
+import { Side } from "./caesarCipher";
 export const alphabetLowerCase = "abcdefghijklmnopqrstuvwxyz";
 
 export const encodeASCII = (letter: string) => {
@@ -7,4 +8,16 @@ export const encodeASCII = (letter: string) => {
 
 export const decodeASCII = (index: number) => {
     return alphabetLowerCase.charAt(index - 1);
+}
+
+// TODO: Find a design pattern to the defin side.Right with position > 0
+// and side.Left with position < 0
+// because if its side.Right position = 120 that's going to positive infinite
+// same as side.Left position = -120 that's going to negative infinite
+export function inRangeASCII(position: number, side: Side): number {
+    if (position > 0 && position < 27) {
+        return position;
+    }
+
+    return inRangeASCII(position + ((side * -1) * 26), side)
 }
--- test/utils/ascii.test.ts
+++ test/utils/ascii.test.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "vitest";
-import { encodeASCII, decodeASCII } from "../../src/utils/ascii";
+import { encodeASCII, decodeASCII, inRangeASCII } from "../../src/utils/ascii";
+import { Side } from "../../src/utils/caesarCipher";
 
 test("encode letters to code ASCII", ()=>{
     expect( encodeASCII("a") ).toEqual(1);
@@ -18,4 +19,15 @@ test("decode code ASCII", () => {
 test("decode code ASCII unexpected numbers obtain empty String ", () => {
     expect(decodeASCII(-1)).toEqual("")
     expect(decodeASCII(27)).toEqual("")
+})
+
+
+test("inRangeASCII expecte to specific long ciphers", ()=>{
+    expect(inRangeASCII(52, Side.Right)).toEqual(26)
+    expect(inRangeASCII(126, Side.Right)).toEqual(22)
+    expect(inRangeASCII(27, Side.Right)).toEqual(1)
+    expect(inRangeASCII(1, Side.Left)).toEqual(1)
+    expect(inRangeASCII(1, Side.Right)).toEqual(1)
+    expect(inRangeASCII(120, Side.Right)).toEqual(16)
+    expect(inRangeASCII(-120, Side.Left)).toEqual(10)
 })
