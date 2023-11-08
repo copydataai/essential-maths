f2efebaf8e5173783314e3f77e5273ea9cf43363
Jose Sanchez <engineer@copydataai.com>
2023-11-08 13:09:53 +0000

    add: decodeCaesarCipher & encodeCaesarCipher

--- src/utils/caesarCipher.ts
+++ src/utils/caesarCipher.ts
@@ -1,26 +1,37 @@
-import { encodeToASCII } from "./ascii";
+import { decodeASCII, encodeASCII } from "./ascii";
 
 export enum Side {
     Left = -1,
     Right = 1,
 }
 
-export function encodeCaesarCipher(text: string, shift: number, side: Side): Array<number> {
-    let positions: Array<number> = [];
+export function encodeCaesarCipher(text: string, shift: number, side: Side): string {
+    let encode = "";
     for (const letter of text) {
-        const position = encodeToASCII(letter);
+        const position = encodeASCII(letter);
         if (!(position > 0)) {
+            encode = encode.concat((letter == " " || letter == "\n") ? letter : "");
             continue;
         }
 
         const encodeSalt = shift * side;
         let positionSalt = encodeSalt + position;
         if (side == Side.Left) {
-            positions.push(positionSalt < 1 ? positionSalt + 26 : positionSalt);
+            encode = encode.concat(positionSalt < 1 ? decodeASCII(positionSalt + 26) : decodeASCII(positionSalt));
             continue
         }
-        positions.push(positionSalt > 26 ? positionSalt - 26 : positionSalt);
+
+        encode = encode.concat(positionSalt > 26 ? decodeASCII(positionSalt - 26) : decodeASCII(positionSalt));
     }
 
-    return positions;
+    return encode;
+}
+
+// It's just a encode function but changing the side
+export function decodeCaesarCipher(cipher: string, shift: number, side: Side){
+    if (side == Side.Left){
+        return encodeCaesarCipher(cipher, shift, Side.Right)
+    }
+
+    return encodeCaesarCipher(cipher, shift, Side.Left)
 }
--- test/utils/caesarCipher.test.ts
+++ test/utils/caesarCipher.test.ts
@@ -1,19 +1,36 @@
 import {test, expect} from "vitest"
-import { encodeCaesarCipher, Side } from "../../src/utils/caesarCipher"
+import { encodeCaesarCipher, decodeCaesarCipher, Side } from "../../src/utils/caesarCipher"
 
 test("encodeCaesarCipher with shift 4 direction right", () => {
-    expect(encodeCaesarCipher("abcd", 4, Side.Right)).toEqual([5,6,7,8])
+    expect(encodeCaesarCipher("abcd", 4, Side.Right)).toEqual('efgh')
 })
 
 test("encodeCaesarCipher with extra caracters direction right", () => {
-    expect(encodeCaesarCipher("Hello world", 4, Side.Right)).toEqual([12,9, 16, 16, 19, 1, 19, 22, 16, 8])
+    expect(encodeCaesarCipher("Hello world", 4, Side.Right)).toEqual("lipps asvph")
 })
 
 test("encodeCaesarCipher with extra caracters direction left", () => {
-    expect(encodeCaesarCipher("Hello world", 4, Side.Left)).toEqual([4,1, 8, 8, 11, 19, 11, 14, 8, 26])
-    expect(encodeCaesarCipher("Hello $ !@$ world @!$!", 4, Side.Left)).toEqual([4,1, 8, 8, 11, 19, 11, 14, 8, 26])
+    expect(encodeCaesarCipher("Hello world", 4, Side.Left)).toEqual("dahhk sknhz")
+    expect(encodeCaesarCipher("Hello $ !@$ world @!$!", 4, Side.Left)).toEqual("dahhk   sknhz ")
 })
 
-test("encodeCaesarCipher with shift 4 direction left", () => {
-    expect(encodeCaesarCipher("abcd", 4, Side.Left)).toEqual([23,24,25,26])
+test("encodeCaesarCipher with side left", () => {
+    expect(encodeCaesarCipher("abcd", 4, Side.Left)).toEqual("wxyz")
+})
+
+test("decodeCaesarCipher with shift 4 direction with previous side right", () => {
+    expect(decodeCaesarCipher("efgh", 4, Side.Right)).toEqual("abcd")
+})
+
+test("decodeCaesarCipher with extra caracters with previous side right", () => {
+    expect(decodeCaesarCipher("lipps asvph", 4, Side.Right)).toEqual("hello world")
+})
+
+test("decodeCaesarCipher with extra caracters direction left", () => {
+    expect(decodeCaesarCipher("dahhk sknhz", 4, Side.Left)).toEqual("hello world");
+    expect(decodeCaesarCipher("dahhk   sknhz ", 4, Side.Left)).toEqual("hello   world ");
+})
+
+test("decodeCaesarCipher with shift 4 direction left", () => {
+    expect(decodeCaesarCipher("wxyz", 4, Side.Left)).toEqual("abcd")
 })
