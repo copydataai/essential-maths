2cdf65ce0955bf8fcf5510e26c4d4216d167ccb0
Jose Sanchez <engineer@copydataai.com>
2023-11-08 13:08:33 +0000

    add: decodeASCII and improve legibility

--- src/utils/ascii.ts
+++ src/utils/ascii.ts
@@ -1,7 +1,10 @@
 export const alphabetLowerCase = "abcdefghijklmnopqrstuvwxyz";
 
-export const encodeToASCII = (letter: string) => {
-    const index = alphabetLowerCase.indexOf(letter.toLowerCase())
-       return index > -1 ? index + 1: index;
+export const encodeASCII = (letter: string) => {
+    const index = alphabetLowerCase.indexOf(letter.toLowerCase());
+    return index > -1 ? index + 1: index;
 }
 
+export const decodeASCII = (index: number) => {
+    return alphabetLowerCase.charAt(index - 1);
+}
--- test/utils/ascii.test.ts
+++ test/utils/ascii.test.ts
@@ -1,11 +1,21 @@
 import { test, expect } from "vitest";
-import { encodeToASCII } from "../../src/utils/ascii";
+import { encodeASCII, decodeASCII } from "../../src/utils/ascii";
 
 test("encode letters to code ASCII", ()=>{
-    expect( encodeToASCII("a") ).toEqual(1);
-    expect( encodeToASCII("z") ).toEqual(26);
+    expect( encodeASCII("a") ).toEqual(1);
+    expect( encodeASCII("z") ).toEqual(26);
 })
 test("receive -1 to unexpected caracters", ()=>{
-    expect( encodeToASCII("$") ).toEqual(-1);
-    expect( encodeToASCII("%") ).toEqual(-1);
+    expect( encodeASCII("$") ).toEqual(-1);
+    expect( encodeASCII("%") ).toEqual(-1);
+})
+
+test("decode code ASCII", () => {
+    expect(decodeASCII(1)).toEqual("a")
+    expect(decodeASCII(5)).toEqual("e")
+})
+
+test("decode code ASCII unexpected numbers obtain empty String ", () => {
+    expect(decodeASCII(-1)).toEqual("")
+    expect(decodeASCII(27)).toEqual("")
 })
