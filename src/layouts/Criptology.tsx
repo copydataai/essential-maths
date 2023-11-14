620ae25eff25b1a3421552f59d5d152784f0a93a
Jose Sanchez <engineer@copydataai.com>
2023-11-14 20:12:48 +0000

    add: layout Criptology

---
+++ src/layouts/Criptology.tsx
@@ -0,0 +1,54 @@
+import { createSignal, createMemo, createEffect, Show } from 'solid-js';
+import './Criptology.css';
+import { Caesar } from '../components/Caesar';
+import { Vigenere } from '../components/Vigenere';
+import { CriptoToggleButton, OptionCripto } from '../components/CriptoToggleButton'
+import { decodeCaesarCipher, encodeCaesarCipher, Side } from '../utils/caesarCipher';
+import { encodeVigenereCipher, decodeVigenereCipher } from '../utils/vigenereCipher';
+
+export function CriptologyLayout() {
+    const [ option, setOption ] = createSignal<OptionCripto>(OptionCripto.CaesarCipher);
+    const [ keypass, setKeypass ] = createSignal<string>("");
+    const [ shift, setShift ] = createSignal<number>(0);
+    const [ side, setSide ] = createSignal<Side>(Side.Right);
+    const [ caesarCipher, setCaesarCipher ] = createSignal<string>("")
+    const [ vigenereCipher, setVigenereCipher ] = createSignal<string>("")
+
+
+    const quoteJuliusCaesar = "I came, I saw and I conquered by Julius Caesar."
+    const quoteVigenere = "The Arts are learnt by reason and method; they are mastered by practice. by Leon Battista Alberti.";
+
+    const cipherText = createMemo<string>(() => {
+        if (caesarCipher().length > 0 && option() == OptionCripto.CaesarCipher) {
+            return caesarCipher();
+        }
+        if (vigenereCipher().length > 0 && option() == OptionCripto.VigenereCipher){
+            return vigenereCipher();
+        }
+    })
+
+    const cipherMemo = createMemo<string>(() => {
+        if (option() == OptionCripto.CaesarCipher && cipherText()) {
+            return encodeCaesarCipher(cipherText(), shift(), side());
+        }
+        if (option() == OptionCripto.VigenereCipher && cipherText()) {
+            return encodeVigenereCipher(cipherText(), keypass());
+        }
+    })
+
+
+    return (
+        <>
+            <div class="cripto-container">
+                <CriptoToggleButton option={option} setOption={setOption}  />
+                <Show when={option() === OptionCripto.CaesarCipher} >
+                    <Caesar placeholder={quoteJuliusCaesar} setCipher={setCaesarCipher} setShift={setShift} setSide={setSide} />
+                </Show>
+                <Show when={option() === OptionCripto.VigenereCipher}>
+                    <Vigenere placeholder={quoteVigenere} setCipher={setVigenereCipher} setKeypass={setKeypass} />
+                </Show>
+                <textarea name="output-code" id="" rows="" cols="" tabindex="" disabled>{cipherMemo}</textarea>
+            </div>
+        </>
+    );
+}
