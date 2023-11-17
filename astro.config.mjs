bf977314aa1a8873b54eb26b33c904d4cbb088b8
Jose Sanchez <engineer@copydataai.com>
2023-11-17 19:20:44 +0000

    add: vercel to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
