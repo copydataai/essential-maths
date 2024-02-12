4d675865961931a7ad53fc42bfd57026c0d9850c
Jose Sanchez <engineer@copydataai.com>
2024-02-12 20:06:11 +0000

    feat: add scatter plot reactive

--- src/layouts/Statistics.tsx
+++ src/layouts/Statistics.tsx
@@ -1,41 +1,80 @@
-import { createSignal, createMemo } from "solid-js";
+import { createSignal, createMemo, onMount } from "solid-js";
+import { ScatterPlot } from "../components/ScatterPlot";
 
 export function StatisticsLayout() {
-  const [xAxis, setXAxis] = createSignal<Array<number>>([]);
-  const [yAxis, setYAxis] = createSignal<Array<number>>([]);
+  const [xAxis, setXAxis] = createSignal<Array<number>>([1, 2, 3, 4]);
+  const [yAxis, setYAxis] = createSignal<Array<number>>([1, 2, 3, 4]);
+
+  const [xTitle, setXTitle] = createSignal<string>("age");
+  const [yTitle, setYTitle] = createSignal<string>("time");
+
+  const dataset = createMemo(() => {
+    const x = xAxis();
+    const y = yAxis();
+    const length = Math.max(x.length, y.length);
+    const results = new Array(length);
+
+    // a loop to iterate the length an create a new array of with x and y values
+    for (let i = 0; i < length; i++) {
+      results[i] = { x: parseInt(x[i]), y: parseInt(y[i]) };
+    }
+    return results;
+  });
 
   return (
     <main class="flex flex-col items-center space-y-2">
       <div class="flex flex-col space-y-2">
         <div class="flex flex-col space-y-2">
           <div class="space-x-2">
-            <label htmlFor="x-axis">name</label>
+            <label htmlFor="x-axis">x-axis</label>
             <input
               class="input input-secondary input-xs"
               type="text"
               name="x-axis"
-              value=""
+              value={xTitle()}
+              onChange={e => {
+                setXTitle(e.target.value);
+              }}
             />
           </div>
           <textarea
             class="textarea textarea-primary"
-            onInput={() => {}}
+            value={xAxis().toString()}
+            onChange={e => {
+              const value = e.target.value;
+              const data = value.replace(/[^,\d]/g, "").split(",");
+              setXAxis(data);
+            }}
           ></textarea>
         </div>
         <div class="flex flex-col space-y-2">
           <div class="space-x-2">
-            <label htmlFor="x-axis">name</label>
+            <label htmlFor="y-axis">y-axis</label>
             <input
               class="input input-secondary input-xs"
               type="text"
-              name="x-axis"
-              value=""
+              name="y-axis"
+              value={yTitle()}
+              onChange={e => {
+                setYTitle(e.target.value);
+              }}
             />
           </div>
-          <textarea class="textarea textarea-primary"></textarea>
+          <textarea
+            class="textarea textarea-primary"
+            value={yAxis().toString()}
+            onChange={e => {
+              const value = e.target.value;
+              setYAxis(value.replace(/[^,\d]/g, "").split(","));
+            }}
+          ></textarea>
         </div>
       </div>
-      <div class=""></div>
+      <ScatterPlot
+        data={dataset()}
+        xAxisTitle={xTitle()}
+        yAxisTitle={yTitle()}
+      />
     </main>
   );
 }
