f8e698cd347ad262050104fc0f5f864b6bf04d6b
Jose Sanchez <engineer@copydataai.com>
2024-02-13 22:33:39 +0000

    feat: boxplot in Statistics

--- src/layouts/Statistics.tsx
+++ src/layouts/Statistics.tsx
@@ -1,5 +1,6 @@
-import { createSignal, createMemo, onMount } from "solid-js";
+import { createSignal, createMemo, onMount, Show } from "solid-js";
 import { ScatterPlot } from "../components/ScatterPlot";
+import { BoxPlot } from "../components/BoxPlot";
 
 export function StatisticsLayout() {
   const [xAxis, setXAxis] = createSignal<Array<number>>([1, 2, 3, 4]);
@@ -7,6 +8,8 @@ export function StatisticsLayout() {
 
   const [xTitle, setXTitle] = createSignal<string>("age");
   const [yTitle, setYTitle] = createSignal<string>("time");
+
+  const [itsCartesian, setItsCartesian] = createSignal<boolean>(false);
 
   const dataset = createMemo(() => {
     const x = xAxis();
@@ -21,62 +24,119 @@ export function StatisticsLayout() {
     return results;
   });
 
+  const boxplotDataset = createMemo(() => {
+    const y = yAxis();
+
+    // return an array with the values of the y axis
+    const results = new Array(y.length);
+
+    for (let i = 0; i < y.length; i++) {
+      results[i] = { y: parseInt(y[i]) };
+    }
+
+    return results;
+  });
+
   return (
     <main class="flex flex-col items-center space-y-2">
-      <div class="flex flex-col space-y-2">
-        <div class="flex space-x-5">
-          <div class="flex flex-col space-y-2">
-            <div class="space-x-2">
-              <label htmlFor="x-axis">x-axis</label>
-              <input
-                class="input input-secondary input-xs"
-                type="text"
-                name="x-axis"
-                value={xTitle()}
-                onChange={e => {
-                  setXTitle(e.target.value);
-                }}
-              />
+      <div class="form-control flex items-center">
+        <label class="label cursor-pointer space-x-2">
+          <span class="label-text">Boxplot</span>
+          <input
+            type="checkbox"
+            class="toggle toggle-primary"
+            onClick={() => setItsCartesian(cartesian => !cartesian)}
+          />
+          <span class="label-text">ScatterPlot</span>
+        </label>
+      </div>
+      <Show
+        when={itsCartesian()}
+        fallback={
+          <div class="flex flex-col items-center space-y-2">
+            <div class="flex flex-col space-y-2">
+              <div class="flex flex-col space-y-2">
+                <div class="space-x-2">
+                  <label htmlFor="y-axis">y-axis</label>
+                  <input
+                    class="input input-secondary input-xs"
+                    type="text"
+                    name="y-axis"
+                    value={yTitle()}
+                    onChange={e => {
+                      setYTitle(e.target.value);
+                    }}
+                  />
+                </div>
+                <textarea
+                  class="textarea textarea-primary"
+                  value={yAxis().toString()}
+                  onChange={e => {
+                    const value = e.target.value;
+                    setYAxis(value.replace(/[^,\d-]/g, "").split(","));
+                  }}
+                ></textarea>
+              </div>
             </div>
-            <textarea
-              class="textarea textarea-primary"
-              value={xAxis().toString()}
-              onChange={e => {
-                const value = e.target.value;
-                const data = value.replace(/[^,\d-]/g, "").split(",");
-                setXAxis(data);
-              }}
-            ></textarea>
+            <BoxPlot data={boxplotDataset()} yAxisTitle={yTitle()} />
           </div>
-          <div class="flex flex-col space-y-2">
-            <div class="space-x-2">
-              <label htmlFor="y-axis">y-axis</label>
-              <input
-                class="input input-secondary input-xs"
-                type="text"
-                name="y-axis"
-                value={yTitle()}
+        }
+      >
+        <div class="flex flex-col space-y-2">
+          <div class="flex space-x-5">
+            <div class="flex flex-col space-y-2">
+              <div class="space-x-2">
+                <label htmlFor="x-axis">x-axis</label>
+                <input
+                  class="input input-secondary input-xs"
+                  type="text"
+                  name="x-axis"
+                  value={xTitle()}
+                  onChange={e => {
+                    setXTitle(e.target.value);
+                  }}
+                />
+              </div>
+              <textarea
+                class="textarea textarea-primary"
+                value={xAxis().toString()}
                 onChange={e => {
-                  setYTitle(e.target.value);
+                  const value = e.target.value;
+                  const data = value.replace(/[^,\d-]/g, "").split(",");
+                  setXAxis(data);
                 }}
-              />
+              ></textarea>
             </div>
-            <textarea
-              class="textarea textarea-primary"
-              value={yAxis().toString()}
-              onChange={e => {
-                const value = e.target.value;
-                setYAxis(value.replace(/[^,\d-]/g, "").split(","));
-              }}
-            ></textarea>
+            <div class="flex flex-col space-y-2">
+              <div class="space-x-2">
+                <label htmlFor="y-axis">y-axis</label>
+                <input
+                  class="input input-secondary input-xs"
+                  type="text"
+                  name="y-axis"
+                  value={yTitle()}
+                  onChange={e => {
+                    setYTitle(e.target.value);
+                  }}
+                />
+              </div>
+              <textarea
+                class="textarea textarea-primary"
+                value={yAxis().toString()}
+                onChange={e => {
+                  const value = e.target.value;
+                  setYAxis(value.replace(/[^,\d-]/g, "").split(","));
+                }}
+              ></textarea>
+            </div>
           </div>
         </div>
-      </div>
-      <ScatterPlot
-        data={dataset()}
-        xAxisTitle={xTitle()}
-        yAxisTitle={yTitle()}
-      />
+        <ScatterPlot
+          data={dataset()}
+          xAxisTitle={xTitle()}
+          yAxisTitle={yTitle()}
+        />
+      </Show>
     </main>
   );
 }
